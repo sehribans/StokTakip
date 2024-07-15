3e0ec650449ae86d327ff8e5d1a2e1a2ba2e0b68
Sehriban SERTDAL <sehribansertdal@gmail.com>
2024-07-15 21:57:58 +0300

    hhh

---
+++ StokTakip/Controllers/DepoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StokTakip.Controllers
+{
+    public class DepoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DepoForm() { return View(); }
+
+
+    }
+}
